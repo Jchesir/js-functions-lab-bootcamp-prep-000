753befb52ead8927b12c1ab2f88d4e318197a086
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-02-14 16:35:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function happyHolidays(){       
   return "Happy holidays!"      }
 
-function happyHolidayTo(Glen)(){
-return "Happy Hannukah, Glen!"
+function happyHolidayToGlen(){
+return "Happy Hannukah, Glen}!"
 }
